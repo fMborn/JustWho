40b9796f016fe130055c1ffe228d848906014469
Su-Nam Park <duverdammterpenner@gmail.com>
2022-02-05 21:01:15 +0100

    Some elasticsearch async client future stuff

--- src/main/java/JustWho/services/IndexService.java
+++ src/main/java/JustWho/services/IndexService.java
@@ -1,35 +1,40 @@
 package JustWho.services;
 
 import JustWho.util.Constants;
+import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class IndexService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexService.class);
 
     @Inject
-    RestHighLevelClient client;
+    ElasticsearchAsyncClient elasticsearchAsyncClient;
 
-    public String fillIndex() throws Exception {
-
+    public String fillIndex() {
         final Map<String, Object> searchIndexMap = new HashMap<>();
-        final String name = "Spider-Man: No Way Home";
+        final String name = "Kung Pow: Enter the Fist";
         searchIndexMap.put("name", name);
-        searchIndexMap.put("genres", List.of("Action, Abenteuer, Science Fiction"));
-        searchIndexMap.put("year", 2021);
-        searchIndexMap.put("ranking", 68);
+        searchIndexMap.put("genres", List.of("Action, Abenteuer, Comedy"));
+        searchIndexMap.put("year", 2002);
+        searchIndexMap.put("ranking", 1);
 
-        IndexRequest request = new IndexRequest(Constants.SEARCH_INDEX).source(searchIndexMap);
-        IndexResponse searchIndexResponse =  client.index(request, RequestOptions.DEFAULT);
+        try {
+            CompletableFuture<IndexResponse> indexResponseFuture = elasticsearchAsyncClient.index(
+                    indexRequest -> indexRequest.index(Constants.SEARCH_INDEX).document(searchIndexMap)
+            );
+        } catch(Exception e) {
+            LOGGER.error("Doof!", e);
+        }
 
-        final Map<String, Object> suggestIndexMap = new HashMap<>();
+/*        final Map<String, Object> suggestIndexMap = new HashMap<>();
         suggestIndexMap.put("name", name);
         suggestIndexMap.put("year", 2021);
         suggestIndexMap.put("suggest_name", name);
@@ -38,7 +43,7 @@ public class IndexService {
         IndexResponse suggestIndexResponse =  client.index(autosuggestRequest, RequestOptions.DEFAULT);
 
         System.out.println(searchIndexResponse.status());
-        System.out.println(suggestIndexResponse.status());
+        System.out.println(suggestIndexResponse.status());*/
 
         return "";
     }
