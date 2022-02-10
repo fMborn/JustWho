08ed300456ae23d9468ec3446992d6e51047a7e2
Su-Nam Park <su-nam.park@bechtle.com>
2022-02-10 16:04:31 +0100

    elastic fantastic!

--- src/main/java/JustWho/Module.java
+++ src/main/java/JustWho/Module.java
@@ -1,17 +1,20 @@
 package JustWho;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import io.micronaut.context.annotation.Factory;
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import io.micronaut.context.annotation.Replaces;
 import io.micronaut.elasticsearch.DefaultElasticsearchConfigurationProperties;
 import jakarta.inject.Inject;
-import jakarta.inject.Qualifier;
 import jakarta.inject.Singleton;
-import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -38,9 +41,19 @@ public class Module {
         @Replaces(ElasticsearchAsyncClient.class)
         @Singleton
         ElasticsearchAsyncClient createAsyncClient () {
+            final CredentialsProvider credentialsProvider =
+                    new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY,
+                    new UsernamePasswordCredentials("elastic", "f1c8863ac69cc550695b4b9ee4c9496aff24e687f00bc14d36d3a63b0d36f24b299df89238a9b51fe24763287e31ef5fd46b6f391cfe7d4abef00526de90715"));
 
-
-            RestClient restClient = RestClient.builder(elasticConfig.getHttpHosts()).build();
+            RestClient restClient = RestClient.builder(elasticConfig.getHttpHosts()).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                @Override
+                public HttpAsyncClientBuilder customizeHttpClient(
+                        HttpAsyncClientBuilder httpClientBuilder) {
+                    return httpClientBuilder
+                            .setDefaultCredentialsProvider(credentialsProvider);
+                }
+            }).build();
 
             ElasticsearchTransport transport = new RestClientTransport(
                     restClient, new JacksonJsonpMapper());
--- src/main/java/JustWho/util/Constants.java
+++ src/main/java/JustWho/util/Constants.java
@@ -2,6 +2,6 @@ package JustWho.util;
 
 public class Constants {
 
-    public static final String SEARCH_INDEX = "movies-index";
+    public static final String SEARCH_INDEX = "movies";
     public static final String SUGGEST_INDEX = "movies_autosuggest";
 }
